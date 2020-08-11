cd04da8ff77d51c0a45b7dec72e314ade6a7ca81
Marta <marta.nunes.abreu@gmail.com>
2020-08-11 21:41:20 +0100

    inserts and selects from independent DB

--- db_manager/main.cc
+++ db_manager/main.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <mysqlx/xdevapi.h>
 
-using ::std::cout;
-using ::std::endl;
 using namespace ::mysqlx;
 
 
@@ -12,96 +10,44 @@ try {
 #warning change pwd
   const char   *url = (argc > 1 ? argv[1] : "mysqlx://root:pwd@127.0.0.1");
 
-  cout << "Creating session on " << url
-       << " ..." << endl;
+  std::cout << "Creating session on " << url
+       << " ..." << std::endl;
 
   Session sess(url);
 
-  cout <<"Session accepted, creating collection..." <<endl;
+  std::cout <<"Session accepted, creating collection..." <<std::endl;
 
-  Schema sch= sess.getSchema("test");
-  Collection coll= sch.createCollection("c1", true);
+  Schema sch= sess.getSchema("password_manager");
 
-  cout <<"Inserting documents..." <<endl;
+  sess.sql("USE password_manager").execute();
+  // sess.sql("CREATE TABLE password_table id INTEGER, account VARCHAR(20), password VARCHAR(50), date_created DATE);");
 
-  coll.remove("true").execute();
+  sess.sql("INSERT INTO password_table (account, password, date_created) VALUES ('dummy2', 'abcdef2', '2020-07-11') ").execute();
+  std::cout <<"adding to database..."<< std::endl;
 
-  {
-    DbDoc doc(R"({ "name": "foo", "age": 1 })");
 
-    Result add =
-      coll.add(doc)
-          .add(R"({ "name": "bar", "age": 2, "toys": [ "car", "ball" ] })")
-          .add(R"({ "name": "bar", "age": 2, "toys": [ "car", "ball" ] })")
-          .add(R"({
-                 "name": "baz",
-                  "age": 3,
-                 "date": { "day": 20, "month": "Apr" }
-              })")
-          .add(R"({ "_id": "myuuid-1", "name": "foo", "age": 7 })")
-          .execute();
+  RowResult res = sess.sql("SELECT account, password from password_table").execute();
 
-    std::list<string> ids = add.getGeneratedIds();
-    for (string id : ids)
-      cout <<"- added doc with id: " << id <<endl;
+  Row row;
+  while ((row = res.fetchOne())) {
+  std::cout << "Account: " << row[0] << std::endl;
+  std::cout << " Password: " << row[1] << std::endl;
   }
 
-  cout <<"Fetching documents..." <<endl;
-
-  DocResult docs = coll.find("age > 1 and name like 'ba%'").execute();
-
-  int i = 0;
-  for (DbDoc doc : docs)
-  {
-    cout <<"doc#" <<i++ <<": " <<doc <<endl;
-
-    for (Field fld : doc)
-    {
-      cout << " field `" << fld << "`: " <<doc[fld] << endl;
-    }
-
-    string name = doc["name"];
-    cout << " name: " << name << endl;
-
-    if (doc.hasField("date") && Value::DOCUMENT == doc.fieldType("date"))
-    {
-      cout << "- date field" << endl;
-      DbDoc date = doc["date"];
-      for (Field fld : date)
-      {
-        cout << "  date `" << fld << "`: " << date[fld] << endl;
-      }
-      string month = doc["date"]["month"];
-      int day = date["day"];
-      cout << "  month: " << month << endl;
-      cout << "  day: " << day << endl;
-    }
-
-    if (doc.hasField("toys") && Value::ARRAY == doc.fieldType("toys"))
-    {
-      cout << "- toys:" << endl;
-      for (auto toy : doc["toys"])
-      {
-        cout << "  " << toy << endl;
-      }
-    }
-
-    cout << endl;
-  }
-  cout <<"Done!" <<endl;
+  std::cout <<"Done!" << std::endl;
 }
 catch (const mysqlx::Error &err)
 {
-  cout <<"ERROR: " <<err <<endl;
+  std::cout <<"ERROR: " <<err << std::endl;
   return 1;
 }
 catch (std::exception &ex)
 {
-  cout <<"STD EXCEPTION: " <<ex.what() <<endl;
+  std::cout <<"STD EXCEPTION: " <<ex.what() << std::endl;
   return 1;
 }
 catch (const char *ex)
 {
-  cout <<"EXCEPTION: " <<ex <<endl;
+  std::cout << "EXCEPTION: " <<ex << std::endl;
   return 1;
 }
